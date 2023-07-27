b2798b35684a35d6ab72efea0d083377ee9394ad
anthonynmh <anthonyneomh@gmail.com>
2023-07-28 00:22:48 +0700

    Expand map

--- script.js
+++ script.js
@@ -2,136 +2,162 @@ let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeapon = 0;
-let fighting;
+let currentArmour = 0;
 let monsterHealth;
 let monsterAttack;
-let inventory = ["stick"];
+let inventory = [" "];
 
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const text = document.querySelector("#text");
-const xpText = document.querySelector("#xpText");
-const healthText = document.querySelector("#healthText");
+// player stats
+const hpText = document.querySelector("#hpText");
 const goldText = document.querySelector("#goldText");
+const xpText = document.querySelector("#xpText");
+const levelText = document.querySelector("#levelText");
+const currentWeaponText = document.querySelector("#currentWeaponText");
+const attackDamageText = document.querySelector("#attackDamageText");
+const armourPowerText = document.querySelector("#armourPowerText");
+const locationText = document.querySelector("#locationText");
+const inventoryText = document.querySelector("#inventoryText");
+
+// monster stats
 const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// controls
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+
+// text
+const text = document.querySelector("#text");
+
 const weapons = [
   {
-    name: "stick",
+    name: "Stick",
     power: 5
   },
   {
-    name: "dagger",
+    name: "Dagger",
     power: 30
   },
   {
-    name: "claw hammer",
+    name: "Rusty Sword",
     power: 50
   },
   {
-    name: "sword",
+    name: "Hunter's Sword",
     power: 100
   },
 ];
 
 const locations = [
   {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"store\"."
+    name: "Intersection",
+    "button text": ["Go to Town Square", "Go to Monster Mines", "Go to Dragon's Den", "Go to Pier"],
+    "button functions": [goToTownSquare, goToMonsterMines, goToDragonDen, goToPier],
+    text: "Which road will you take at this intersection?"
   },
   {
-    name: "store",
-    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions": [buyHealth, buyWeapon, goToTownSquare],
-    text: "You entered the store."
+    name: "Town Square",
+    "button text": ["Go to Weapon Store", "Go to Armour Store", "Go to Infirmary", "Exit Town Square"],
+    "button functions": [goToWeaponStore, goToArmourStore, goToInfirmary, goToIntersection],
+    text: "You are in the town square. Are you in need of weapons, armour, or healthcare?"
   },
   {
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Run"],
-    "button functions": [fightSlime, fightBeast, goToTownSquare],
-    text: "You entered the cave. Monsters surround you."
+    name: "Weapon Store",
+    "button text": ["Buy weapon (30 gold)", "Sell weapon", "Exit store", " "],
+    "button functions": [buyWeapon, sellWeapon, goToTownSquare, null],
+    text: "You entered the weapon store."
   },
   {
-    name: "infirmary",
-    "button text": ["Pay for healthcare (10 gold)", " ", " "],
-    "button functions": [reset, null, null],
-    text: "You fainted during the intense fight. Try again next time."
+    name: "Armour Store",
+    "button text": ["Buy armour (25 gold)", "Sell armour", "Exit store", " "],
+    "button functions": [buyArmour, sellArmour, goToTownSquare, null],
+    text: "You entered the armour store."
   },
   {
-    name: "slime hole",
-    "button text": ["Attack", "Block", "Run"],
-    "button functions": [attack, block, run],
-    text: "You engage the slime."
+    name: "Infirmary",
+    "button text": ["Buy HP (20 gold)", "Exit store", " ", " "],
+    "button functions": [buyHp, goToTownSquare, null, null],
+    text: "You entered the Infirmary."
   },
   {
-    name: "Beast arena",
-    "button text": ["Attack", "Block", "Run"],
-    "button functions": [attack, block, run],
-    text: "You engage the beast."
+    name: "Monster Mines",
+    "button text": ["Fight monsters", "Turn around", " ", " "],
+    "button functions": [fightMonsters, goToIntersection, null, null],
+    text: "You stand in front of Monster Mines. Monsters begin to swarm you."
+  },
+  {
+    name: "Dragon's Den",
+    "button text": ["Fight Dragon", "Turn around", " ", " "],
+    "button functions": [fightDragon, goToIntersection, null, null],
+    text: "You stand in front of the Dragon's Den. You feel bad aura from the inside."
+  },
+  {
+    name: "Pier",
+    "button text": ["Return to Intersection", " ", " ", " "],
+    "button functions": [goToIntersection, null, null, null],
+    text: "You are at the nicest pier you have ever seen."
   }
 ];
 
 // initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
+button1.onclick = goToTownSquare;
+button2.onclick = goToMonsterMines;
+button3.onclick = goToDragonDen;
+button4.onclick = goToPier;
 
 function update(location) {
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
+  button4.innerText = location["button text"][3];
 
+  locationText.innerText = location["name"];
   text.innerText = location["text"];
 
   button1.onclick = location["button functions"][0];
   button2.onclick = location["button functions"][1];
   button3.onclick = location["button functions"][2];
+  button4.onclick = location["button functions"][3];
 }
+
+// goTo functions
+function goToIntersection() {
+  update(locations[0]);
+} 
 
 function goToTownSquare() {
-  update(locations[0]);
-}
-
-function goStore() {
   update(locations[1]);
 }
 
-function goCave() {
+function goToWeaponStore() {
   update(locations[2]);
 }
 
-function goInfirmary() {
+function goToArmourStore() {
   update(locations[3]);
 }
 
-function fightSlime() {
-  monsterStats.style.display = "block";
-
-  monsterAttack = 5;
-  monsterHealth = 100;
-  monsterNameText.innerText = "slimey";
-  monsterHealthText.innerText = monsterHealth;
-
+function goToInfirmary() {
   update(locations[4]);
 }
 
-function fightBeast() {
-  monsterStats.style.display = "block";
-
-  monsterAttack = 15;
-  monsterHealth = 150;
-  monsterNameText.innerText = "beasty";
-  monsterHealthText.innerText = monsterHealth;
-
+function goToMonsterMines() {
   update(locations[5]);
 }
 
-function buyHealth() {
+function goToDragonDen() {
+  update(locations[6]);
+}
+
+function goToPier() {
+  update(locations[7]);
+}
+
+// store functions
+function buyHp() {
   if (gold >= 10) {
     health += 10;
     gold -= 10;
@@ -172,6 +198,41 @@ function sellWeapon() {
   }
 }
 
+function buyArmour() {
+
+}
+
+function sellArmour() {
+
+}
+
+// combat functions
+function fightMonsters() {
+
+}
+
+function fightSlime() {
+  monsterStats.style.display = "block";
+
+  monsterAttack = 5;
+  monsterHealth = 100;
+  monsterNameText.innerText = "slimey";
+  monsterHealthText.innerText = monsterHealth;
+
+  update(locations[4]);
+}
+
+function fightBeast() {
+  monsterStats.style.display = "block";
+
+  monsterAttack = 15;
+  monsterHealth = 150;
+  monsterNameText.innerText = "beasty";
+  monsterHealthText.innerText = monsterHealth;
+
+  update(locations[5]);
+}
+
 function attack() {
   monsterHealth -= weapons[currentWeapon].power;
   text.innerText = "Attack! Dealt " + weapons[currentWeapon].power + " damage! It attacks you with " + monsterAttack + " damage.";
@@ -197,12 +258,13 @@ function block() {
   healthText.innerText = health;
 }
 
-function run() {
-  goToTownSquare();
-}
-
 function fightDragon() {
   console.log("Fighting dragon.");
+}
+
+// other functions
+function run() {
+  goToTownSquare();
 }
 
 function reset() {
